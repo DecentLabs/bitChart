046bdb025d251c455f53d6357a179a4f7d702b77
brigittaforrai <forraibrigi@gmail.com>
2019-09-02 15:26:22 +0200

    Initial Commit

---
+++ bitChartTests/bitChartTests.swift
@@ -0,0 +1,34 @@
+//
+//  bitChartTests.swift
+//  bitChartTests
+//
+//  Created by brigitta forrai on 2019. 09. 02..
+//  Copyright © 2019. brigitta forrai. All rights reserved.
+//
+
+import XCTest
+@testable import bitChart
+
+class bitChartTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
